8c9d1878bfdcd94373cd7746d760ef25be973a7b
Emir <emiryilmazb@gmail.com>
2023-08-03 04:11:59 +0300

    minor changes

--- main.py
+++ main.py
@@ -9,28 +9,35 @@ import btcturk_api.exceptions
 import btcturk_api.properties
 from btcturk_api.client import Client
 
-apiKey = "ade38ced-11aa-41e5-8710-c2ac3da492c9"
-apiSecret = "vnh0AjNTI3E6jNyuEgqbCDpFfl5MsiXe"
-
-apiSecret = base64.b64decode(apiSecret)
-stamp = str(int(time.time())*1000)
-data = "{}{}".format(apiKey, stamp).encode('utf-8')
-signature = hmac.new(apiSecret, data, hashlib.sha256).digest()
-signature = base64.b64encode(signature)
+btc_amount = 2500
+eth_amount = 1875
+atom_amount = 625
+desired_currency = "TRY"  # you can choose your desired currency "USD" or "TRY"
 
 
-headers= {
-  "X-PCK": apiKey,
-  "X-Stamp": stamp,
-  "X-Signature": signature,
-  "Content-Type": "application/json"
-}
+with open("secret.txt") as f:
+    contents = f.read()
+    apiKey = contents.split("\n")[0]  # the api key that you took from btcturk API goes to first line of the secret.txt file
+    apiSecret = contents.split("\n")[1].strip()  # the api secret goes to second line of the secret.txt file
 
-
-my_client = Client(api_key=apiKey, api_secret="vnh0AjNTI3E6jNyuEgqbCDpFfl5MsiXe")
+my_client = Client(api_key=apiKey, api_secret=apiSecret)
 time = my_client.get_server_time()
 balance = my_client.get_account_balance()
-# print(time["serverTime2"])
+newbalance = []
+mycurrency = []
+
+for item in balance:
+    if float(item["balance"]) > 0:
+        newbalance.append(item)
+        mycurrency.append(item["asset"])
+# print(mycurrency)
+
+for item in my_client.tick():
+    # if item["pair"] + desired_currency in mycurrency:
+    if item["pair"].replace(desired_currency,"") in mycurrency:
+        # print(item)
+        pass
+print(newbalance)
 
 
 balance_try = balance[0]["balance"]
@@ -41,10 +48,6 @@ balance_eth = balance[34]["balance"]
 btctry = my_client.tick()[0]["last"]
 ethtry = my_client.tick()[1]["last"]
 atomtry = my_client.tick()[19]["last"]
-
-btc_amount = 2500
-eth_amount = 1875
-atom_amount = 625
 
 
 result_btc = float(balance_btc) * btctry
@@ -57,32 +60,4 @@ result_eth = float(balance_eth) * ethtry
 
 total_tl_amount = result_atom + result_btc + result_eth + float(balance_try)
 
-print(total_tl_amount)
-print(result_btc)
-# print(balance_btc)
-# print(balance_atom)
-# print(balance_eth)
-
-# for item in balance:
-#     if float(item["balance"]) > 0:
-#         print(item)
-
-# for item in my_client.tick():
-#     if item["pair"] == "BTCTRY":
-#         print(item["last"])
-# c = 0
-# for item in my_client.tick():
-#     if item["pair"] == "ATOMTRY":
-#         print(item)
-#         print
-#         break
-#     c +=1
-
-# c = 0
-# for item in balance:
-#     if item["asset"] == "ETH":
-#         print(item)
-#         print(c)
-#         break
-#     c +=1
-
+# print(total_tl_amount)
